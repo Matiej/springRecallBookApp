878854461bd24b8b0989004be493500dd5ba5ba6
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-07-17 18:12:12 +0200

    initial commit.

---
+++ src/test/java/com/testaarosa/spirngRecallBookApp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testaarosa.spirngRecallBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
