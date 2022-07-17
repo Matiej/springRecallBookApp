f1c824d259fe213d9e481df7a0ba76ac88d8655b
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-07-17 19:10:36 +0200

    M2L4 add first bean and recalling git

--- src/main/java/com/testaarosa/spirngRecallBookApp/springRecallBookApp.java
+++ src/main/java/com/testaarosa/spirngRecallBookApp/springRecallBookApp.java
@@ -4,23 +4,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class springRecallBookApp implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(springRecallBookApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(springRecallBookApp.class, args);
+    }
 
-	private final CatalogService catalogService;
+    private final CatalogService catalogService;
 
-	public springRecallBookApp(CatalogService catalogService) {
-		this.catalogService = catalogService;
-	}
+    public springRecallBookApp(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
 
 
-	@Override
-	public void run(String... args) throws Exception {
-//		CatalogService cs = new CatalogService();
-		catalogService.findByTitle("Pan").stream().forEach(System.out::println);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        List<Book> bookList = catalogService.findByTitle("Pan");
+        System.out.println("Amount of books: " + bookList.size());
+        bookList.forEach(book -> System.out.println("Amount of books: " + book.s"Books: " + book));
+    }
 }
