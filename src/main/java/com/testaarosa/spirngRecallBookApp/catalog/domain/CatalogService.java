ae6b2b514ddf2a184dece74e1fd21acd61239e38
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-07-18 22:06:50 +0200

    implementation of another memory data base - bestsellerCatalog, refactor old class name and create SchoolCatalogRepostiory

--- src/main/java/com/testaarosa/spirngRecallBookApp/catalog/domain/CatalogService.java
+++ src/main/java/com/testaarosa/spirngRecallBookApp/catalog/domain/CatalogService.java
@@ -1,6 +1,7 @@
 package com.testaarosa.spirngRecallBookApp.catalog.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CatalogService {
+    @Qualifier("memorySchoolCatalogRepository")
     private final CatalogRepository catalogRepository;
 
     public List<Book> findByTitle(String title) {
