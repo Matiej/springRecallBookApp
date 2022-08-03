d7d976d03931f0783d6520907ff12e1db8cc955e
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-08-03 20:40:27 +0200

    first path variable and methond find by ID implemented

--- src/main/java/com/testaarosa/spirngRecallBookApp/catalog/controller/HttpHeaderFactory.java
+++ src/main/java/com/testaarosa/spirngRecallBookApp/catalog/controller/HttpHeaderFactory.java
@@ -10,10 +10,10 @@ public class HttpHeaderFactory {
     private static final String SUCCESSFUL = "Successful";
 
 
-    public static HttpHeaders getSuccessfulHeaders(HttpMethod... allowMethods) {
+    public static HttpHeaders getSuccessfulHeaders(HttpStatus status, HttpMethod... allowMethods) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, Arrays.toString(allowMethods));
-        httpHeaders.add(HeaderKey.STATUS.getHeaderKeyLabel(), String.valueOf(HttpStatus.OK.value()));
+        httpHeaders.add(HeaderKey.STATUS.getHeaderKeyLabel(), String.valueOf(status));
         httpHeaders.add(HeaderKey.MESSAGE.getHeaderKeyLabel(), SUCCESSFUL);
         return httpHeaders;
     }
