7063d3872a4588ec09d0c15389b652c0bb34f388
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-08-29 20:23:21 +0200

    fix messages and exeption handling.

--- src/main/java/com/testaarosa/spirngRecallBookApp/exceptionHandler/RestControllerExceptionHandler.java
+++ src/main/java/com/testaarosa/spirngRecallBookApp/exceptionHandler/RestControllerExceptionHandler.java
@@ -1,22 +1,30 @@
 package com.testaarosa.spirngRecallBookApp.exceptionHandler;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.validation.ConstraintViolationException;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RestControllerAdvice
 public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
-
 
     //todo every exception handle like below
 //    @ExceptionHandler({Exception.class})
@@ -31,7 +39,8 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String message = "error servlet params ";
+        String message = "Method arguments error";
+        log.error(message, ex);
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
         ExceptionHandlerResponse exceptionHandlerResponse = getExceptionHandlerResponse(ex, message, badRequest);
         return ResponseEntity.status(badRequest)
@@ -42,7 +51,13 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
     private ExceptionHandlerResponse getExceptionHandlerResponse(Exception ex, String message, HttpStatus httpStatus) {
         String details = ex.getMessage();
         if (ex instanceof MethodArgumentNotValidException) {
-            details = ex.getMessage().substring(ex.getMessage().indexOf(":") + 1);
+            List<MethodArgumentErrorDetailMessage> allErrors = ((MethodArgumentNotValidException) ex).getAllErrors()
+                    .stream()
+                    .map(err -> new MethodArgumentErrorDetailMessage(((FieldError) err).getField(),
+                            ((FieldError) err).getRejectedValue(),
+                            err.getDefaultMessage()))
+                    .toList();
+            details = allErrors.toString();
         }
         return new ExceptionHandlerResponse(LocalDateTime.now().withNano(0), message, details, String.valueOf(httpStatus.value()), httpStatus.name());
     }
@@ -54,4 +69,21 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
         return httpHeaders;
     }
 
+}
+
+@Data
+@AllArgsConstructor
+class MethodArgumentErrorDetailMessage {
+    private String fieldName;
+    private Object rejectedValue;
+    private String filedErrorMessage;
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", MethodArgumentErrorDetailMessage.class.getSimpleName() + "[", "]")
+                .add("fieldName='" + fieldName + "'")
+                .add("rejectedValue='" + rejectedValue + "'")
+                .add("filedErrorMessage='" + filedErrorMessage + "'")
+                .toString();
+    }
 }
