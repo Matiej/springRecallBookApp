f38605b0a719f5b37b319ec1370c1d9ba574cde5
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-09-26 15:54:04 +0200

    Add scheduler to change on old orders status.

--- src/main/java/com/testaarosa/springRecallBookApp/order/dataBase/OrderJpaRepository.java
+++ src/main/java/com/testaarosa/springRecallBookApp/order/dataBase/OrderJpaRepository.java
@@ -4,8 +4,10 @@ import com.testaarosa.springRecallBookApp.order.domain.Order;
 import com.testaarosa.springRecallBookApp.order.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByOrderStatus(OrderStatus orderStatus);
+    List<Order> findAllByOrderStatusAndAndCreatedAtLessThanEqual(OrderStatus orderStatus, LocalDateTime timeStamp);
 }
