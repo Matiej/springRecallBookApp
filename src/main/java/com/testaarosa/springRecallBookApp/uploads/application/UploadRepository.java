12950e8c3a2e752ff80dc46e18bfdab300552e41
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-11-24 23:17:22 +0100

    clean imports

--- src/main/java/com/testaarosa/springRecallBookApp/uploads/application/UploadRepository.java
+++ src/main/java/com/testaarosa/springRecallBookApp/uploads/application/UploadRepository.java
@@ -1,7 +1,5 @@
 package com.testaarosa.springRecallBookApp.uploads.application;
 
-import com.testaarosa.springRecallBookApp.uploads.application.*;
-
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/com/testaarosa/springRecallBookApp/user/application/port/UserUseCase.java
+++ src/main/java/com/testaarosa/springRecallBookApp/user/application/port/UserUseCase.java
@@ -1,8 +1,8 @@
 package com.testaarosa.springRecallBookApp.user.application.port;
 
+import com.testaarosa.springRecallBookApp.user.application.RegisterUserCommand;
 import com.testaarosa.springRecallBookApp.user.application.RegisterUserRecipientCommand;
 import com.testaarosa.springRecallBookApp.user.application.RegisterUserResponse;
-import com.testaarosa.springRecallBookApp.user.application.RegisterUserCommand;
 import com.testaarosa.springRecallBookApp.user.application.UserQueryCommand;
 import com.testaarosa.springRecallBookApp.user.domain.UserEntity;
 
